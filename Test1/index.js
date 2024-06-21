d2ef393ab7841e9e795485c6776a945ad4d56d7a
Rahul Yadav <kumarrahul54817@gmail.com>
2024-06-21 21:44:36 +0530

    new fun addProduct added

--- Test1/index.js
+++ Test1/index.js
@@ -1,3 +1,13 @@
 const findSum = (num1, num2) => {
     return num1 + num2;
 }
+
+let products = [];
+
+const addProduct = (name, price) =>{
+    const newProduct = {
+        product_name : name,
+        product_price : price
+    }
+    products = [...products, newProduct]
+}
