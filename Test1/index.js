53c18be55475b5baaa1a1c794e3738c6241b5e35
Rahul Yadav <kumarrahul54817@gmail.com>
2024-06-21 21:39:56 +0530

    add new fun create user

--- Test1/index.js
+++ Test1/index.js
@@ -1,3 +1,14 @@
 const findSum = (num1, num2) => {
     return num1 + num2;
 }
+
+let users = [];
+const createUser = (name, email) =>{
+
+    const newUser = {
+        name : name,
+        email : email
+    }
+    users = [...users, newUser];
+
+}
